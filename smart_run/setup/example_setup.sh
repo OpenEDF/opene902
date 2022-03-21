c4e2df06aced8bfdd16f3dbc2483b81322cd2138
macro <makermuyi@163.com>
2022-03-21 11:14:56 +0800

    Modify the Makefile support verdi and vcs

--- smart_run/setup/example_setup.sh
+++ smart_run/setup/example_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export TOOL_EXTENSION=/home/macro/riscv_toolchain_wujian/bin/
+export TOOL_EXTENSION=/home/macro/riscv_toolchain_xuantie_900/bin/
 echo "Toolchain path($TOOL_EXTENSION):"
 echo "    $TOOL_EXTENSION"
